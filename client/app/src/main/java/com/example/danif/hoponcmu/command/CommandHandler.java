7a8e67c8a07a33867b5a7f74c28795abd3d19c7a
DeXtroTip <xibitix@hotmail.com>
2018-05-12 19:30:16 +0100

    Revert to 4bc71a628d410277aeb82b8a6a4c87ff83ccc6b7

--- client/app/src/main/java/com/example/danif/hoponcmu/command/CommandHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.danif.hoponcmu.command;
-
-import com.example.danif.hoponcmu.client.CommandHandlerImpl;
-import com.example.danif.hoponcmu.response.Response;
-
-public interface CommandHandler {
-    void handle(Command c);
-}
--- servidor/servidor-cliente/server/server/src/pt/ulisboa/tecnico/cmu/command/CommandHandler.java
+++
@@ -1,8 +0,0 @@
-package pt.ulisboa.tecnico.cmu.command;
-
-import pt.ulisboa.tecnico.cmu.response.Response;
-
-public interface CommandHandler {
-	public Response helloCommandHandle(HelloCommand hc);
-	public Response signUpCommandHandle(SignUpCommand sup);
-}
